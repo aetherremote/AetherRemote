e7ccf6819e47f59abc2a1118da9bf4e73623dca7
Mora <157671690+aetherremote@users.noreply.github.com>
2025-04-07 16:35:27 -0500

    Bump server expected version

--- AetherRemoteServer/Controllers/AuthController.cs
+++ AetherRemoteServer/Controllers/AuthController.cs
@@ -16,7 +16,7 @@ namespace AetherRemoteServer.Controllers;
 public class AuthController(Configuration config, DatabaseService db)
     : ControllerBase
 {
-    private readonly Version _expectedVersion = new(2, 1, 1, 0);
+    private readonly Version _expectedVersion = new(2, 2, 0, 0);
 
     [AllowAnonymous]
     [HttpPost("login")]
