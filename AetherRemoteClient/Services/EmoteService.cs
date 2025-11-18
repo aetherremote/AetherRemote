3b039f4c4204cd74a6cba2ce5c0afe58e36e8ce0
Mora <157671690+aetherremote@users.noreply.github.com>
2025-11-17 20:06:55 -0600

    Replace 'stroke' emote with 'pet' to fix crashing

--- AetherRemoteClient/Services/EmoteService.cs
+++ AetherRemoteClient/Services/EmoteService.cs
@@ -19,20 +19,28 @@ public class EmoteService
     /// </summary>
     public EmoteService()
     {
-        var emoteSheet = Plugin.DataManager.Excel.GetSheet<Emote>();
-
-        for (uint i = 0; i < emoteSheet.Count; i++)
+        var emotes = Plugin.DataManager.Excel.GetSheet<Emote>();
+        for (uint i = 0; i < emotes.Count; i++)
         {
-            var emote = emoteSheet.GetRowOrDefault(i);
+            var emote = emotes.GetRowOrDefault(i);
             if (emote is null) continue;
 
             var command = emote.GetValueOrDefault().TextCommand.ValueNullable?.Command.ToString();
             if (command.IsNullOrEmpty()) continue;
-            Emotes.Add(command[1..]);
+            var commandWithoutSlash = command[1..];
+            
+            /*
+             * For some reason, there is a bug in game where if someone is actively doing the /pet emote
+             * and someone in Aether Remote issues them a /stroke command, the game will hard crash to
+             * desktop without any crash logs. So hopefully, just replacing /stroke with /pet will
+             * prevent a situation where this can happen.
+             */
+            Emotes.Add(commandWithoutSlash == "stroke" ? "pet" : commandWithoutSlash);
 
             var shortCommand = emote.GetValueOrDefault().TextCommand.ValueNullable?.ShortCommand.ExtractText();
             if (shortCommand.IsNullOrEmpty()) continue;
-            Emotes.Add(shortCommand[1..]);
+            var shortCommandWithoutSlash = shortCommand[1..];
+            Emotes.Add(shortCommandWithoutSlash == "stroke" ? "pet" : shortCommandWithoutSlash);
         }
 
         Emotes.Sort();
