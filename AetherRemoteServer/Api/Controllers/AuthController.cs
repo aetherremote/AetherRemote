53b57002c2e817c5398741b32b1508b6fd7ac34e
Mora <157671690+aetherremote@users.noreply.github.com>
2025-12-10 13:18:36 -0600

    Bump to 2.8.4.1

--- AetherRemoteServer/Api/Controllers/AuthController.cs
+++ AetherRemoteServer/Api/Controllers/AuthController.cs
@@ -17,7 +17,7 @@ namespace AetherRemoteServer.Api.Controllers;
 public class AuthController(Configuration config, IDatabaseService database) : ControllerBase
 {
     // Const
-    private static readonly Version ExpectedVersion = new(2, 8,4, 0);
+    private static readonly Version ExpectedVersion = new(2, 8,4, 1);
     
     // Instantiated
     private readonly SymmetricSecurityKey _key = new(Encoding.UTF8.GetBytes(config.SigningKey));
