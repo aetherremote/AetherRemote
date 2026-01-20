b92ac84b9e29a7e412c7fde090eabb218dfa44e9
Mora <157671690+aetherremote@users.noreply.github.com>
2026-01-20 02:59:04 -0600

    Correct stray newtonsoft import from Dalamud

--- AetherRemoteServer/SignalR/Hubs/PrimaryHub.Misc.cs
+++ AetherRemoteServer/SignalR/Hubs/PrimaryHub.Misc.cs
@@ -18,7 +18,7 @@ public partial class PrimaryHub
     public async Task<ActionResponse> Honorific(HonorificRequest request)
     {
         var friendCode = FriendCode;
-        LogWithBehavior($"[HonorificRequest] Sender = {friendCode}, Honorific = {request.Honorific}", LogMode.Console);
+        LogWithBehavior($"[HonorificRequest] Sender = {friendCode}, Targets = {string.Join(", ", request.TargetFriendCodes)}, Honorific = {request.Honorific}", LogMode.Console);
         return await honorificHandler.Handle(friendCode, request, Clients);
     }
     
