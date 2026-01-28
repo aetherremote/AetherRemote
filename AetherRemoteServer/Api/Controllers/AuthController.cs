cb38adf2968d432685ab7bb2150cf7c1ae027ffa
Mora <157671690+aetherremote@users.noreply.github.com>
2026-01-28 12:17:51 -0600

    Possession beta release

--- AetherRemoteServer/Api/Controllers/AuthController.cs
+++ AetherRemoteServer/Api/Controllers/AuthController.cs
@@ -17,7 +17,7 @@ namespace AetherRemoteServer.Api.Controllers;
 public class AuthController(Configuration config, IDatabaseService database) : ControllerBase
 {
     // Const
-    private static readonly Version ExpectedVersion = new(2, 8,6, 3);
+    private static readonly Version ExpectedVersion = new(2, 9,0,0);
     
     // Instantiated
     private readonly SymmetricSecurityKey _key = new(Encoding.UTF8.GetBytes(config.SigningKey));
